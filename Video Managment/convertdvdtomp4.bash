ff9b359cdcf49b6b0071f6438e1fb715632eff9c
Damian Galli <da.ga@live.de>
2021-04-03 16:36:42 +0200

    Update convertdvdtomp4.bash

--- Video Managment/convertdvdtomp4.bash
+++ Video Managment/convertdvdtomp4.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat *.VOB > moviename.vob; ffmpeg -i moviename.vob -acodec libfaac -ac 2 -ab 128k -vcodec libx264 -vpre fast -crf 20 -threads 0 moviename.mp4
+cat *.VOB > moviename.vob; ffmpeg -i moviename.vob -c:a aac -ac 2 -ab 128k -vcodec libx264 -preset fast -crf 20 -threads 0 moviename.mp4
